5e4f7e4ffb43d1bc9988bd8197e8cebeec192b04
MichaelGuoWW <mguo62@wisc.edu>
2024-04-23 23:20:47 -0500

    success local UDP

--- UDP_server.py
+++ UDP_server.py
@@ -5,18 +5,45 @@ import numpy as np
 import time
 import base64
 
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
 BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 host_name = socket.gethostname()
-host_ip = '192.168.1.238'#  socket.gethostbyname(host_name)
+host_ip = '192.168.0.142'#  socket.gethostbyname(host_name)
 print(host_ip)
 port = 9999
 socket_address = (host_ip,port)
 server_socket.bind(socket_address)
 print('Listening at:',socket_address)
 
-vid = cv2.VideoCapture(0) #  replace 'rocket.mp4' with 0 for webcam
+vid = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER) 
 fps,st,frames_to_count,cnt = (0,0,20,0)
 
 while True:
---
+++ test_camera.py
@@ -0,0 +1,47 @@
+import cv2
+
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+vid = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER) 
+
+
+import json
+while(True):
+    # Capture frame-by-frame
+    ret, frame = vid.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
